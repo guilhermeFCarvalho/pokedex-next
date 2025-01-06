f2c16326cc49689fec1e84eb489c391323e35836
guilhermeFCarvalho <gui.carvalho021@gmail.com>
2025-01-06 18:36:29 -0300

    chore: style appbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono,Press_Start_2P } from "next/font/google";
-import {  } from "next/font/google";
+import { Geist, Geist_Mono, Press_Start_2P } from "next/font/google";
+import { } from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -14,7 +14,7 @@ const geistMono = Geist_Mono({
 });
 
 const pressStart2P = Press_Start_2P({
-  weight:"400",
+  weight: "400",
   variable: "--font-press-start",
   subsets: ["latin"],
 });
@@ -36,14 +36,21 @@ export default function RootLayout({
       >
         <header
           style={{
-            backgroundColor: "var(--tertiary)",
-            color: "white",
+            backgroundColor: "var(--primary)",
+            color: "#ffcb05",
             padding: "1rem",
             textAlign: "center",
-            fontFamily: "var(--font-press-start)", 
+            fontFamily: "var(--font-press-start)",
           }}
         >
-          <h2>Pokedex</h2>
+          <h2
+            style={{
+              textShadow: "4px 4px 2px #2a75bb", 
+              fontSize: "1.5rem", 
+            }}
+          >
+            Pokédex
+          </h2>
         </header>
 
         <main>{children}</main>
