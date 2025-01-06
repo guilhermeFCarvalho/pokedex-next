3e40cdad24be68e3a52123807c78f29ec388f48e
guilhermeFCarvalho <gui.carvalho021@gmail.com>
2025-01-05 23:31:10 -0300

    fix: fix webview event

--- app/components/card-buttons/card-buttons.tsx
+++ app/components/card-buttons/card-buttons.tsx
@@ -9,7 +9,7 @@ export function CardButtons(pokemon: Pokemon) {
     return (
         <div className={styles.actions}>
             <button className={`${styles.button} ${styles.like}`} onClick={() => window.ReactNativeWebView.postMessage(JSON.stringify({ pokemon, liked: true }))} >Like</button>
-            <button className={`${styles.button} ${styles.dislike}`} onClick={() => window.ReactNativeWebView.postMessage(JSON.stringify({ pokemon, liked: true }))}>Dislike</button>
+            <button className={`${styles.button} ${styles.dislike}`} onClick={() => window.ReactNativeWebView.postMessage(JSON.stringify({ pokemon, liked: false }))}>Dislike</button>
         </div>
     );
 }
